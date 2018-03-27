2069af45b49598118f2c42971f712ef1d2566272
Zoe <zoereagan@gmail.com>
2018-03-27 14:49:05 -0500

    commit one: created tomagotchi class

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,18 @@
 console.log("hello");
+
+//Create a class for your tomagotchi
+
+class Tomagotchi = {
+  constructor(name, age, hunger, sleepiness, boredom)
+  this.name = name;
+  this.age = age;
+  this.hunger = hunger;
+  this.sleepiness = sleepiness;
+  this.boredom = boredom;
+
+}
+
+$('button').on('click', (event) => {
+  const petName = $('input').val();
+  console.log(petName);
+})
